bb53ed957b8956bcf49103b91fc864e73e01a3c1
Elliot Cohen <elliot.cohen@gmail.com>
2011-02-19 16:29:27 -0500

    testing params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  def authenticate_user!
+    
+  end
 end
--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -1,6 +1,4 @@
 class CanvasController < ApplicationController
-  before_filter :authenticate_user!
-  
   def index
     @params = params
     
