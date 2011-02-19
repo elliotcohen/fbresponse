77cdd7803217b7d23dab6b76f8fc685b78891f2c
Elliot Cohen <elliot.cohen@gmail.com>
2011-02-19 13:04:11 -0500

    initial project import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbresponse::Application.config.secret_token = '5daf20de89a2ffffd8fcbd6b76946a38623edb89ec36967db493067428ee05752e6d629e04e9738fe3373fe9b30791e01d3fef0b7ea32cf430be01a50a71295f'
