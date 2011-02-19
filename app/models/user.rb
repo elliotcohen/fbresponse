4ff2142a5434487c6ad5e56402e61a52399497fa
Elliot Cohen <elliot.cohen@gmail.com>
2011-02-19 15:44:00 -0500

    added canvas hellow world

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,17 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
+  def self.find_for_facebook_oauth(data_hash, signed_in_resource=nil)
+    data = data_hash['extra']['user_hash']
+    if user = User.find_by_email(data["email"])
+      if data_hash['credentials']['token'] != user.access_token
+        user.access_token = data_hash['credentials']['token']
+        user.save
+      end
+      user
+    else
+      nil
+    end
+  end
 
 end
