9b70539a82614d8df94b5673eaf9284882ffd27c
Elliot Cohen <elliot.cohen@gmail.com>
2011-02-19 16:36:13 -0500

    redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  def authenticate_user!
-    
+  def sign_user_in
+    if user_signed_in?
+      return true
+    else
+      redirect_to '/users/auth/facebook'
+    end
   end
 end
--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -1,8 +1,8 @@
 class CanvasController < ApplicationController
+  before_filter :sign_user_in
+
   def index
-    @params = params
-    
-#    @user = User.find_for_facebook_oauth(params[:])
+    @user = User.find_for_facebook_oauth(params[:signed_request])
 #    if !signed_in
 #      redirect_to 'https://www.facebook.com/dialog/oauth?client_id=135742356491626&redirect_uri=http://http://smooth-meadow-728.heroku.com/canvas/'
 #    else
