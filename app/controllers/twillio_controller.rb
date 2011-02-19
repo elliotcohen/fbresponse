ca7780db8ad0e4e60d57cb55d27b281aaf7bf0f9
Wai yong Low <wlow@wai-yong-lows-macbook.local>
2011-02-19 16:11:40 -0500

    adding twillio stuff

--- app/controllers/twillio_controller.rb
+++ app/controllers/twillio_controller.rb
@@ -1,11 +1,15 @@
 class TwillioController < ApplicationController
 
   def create
-    render :template => 'hello'
+    User.send_text(params[:From], params[:Body])
+    redirect_to :success
   end
   
   def index
     render :template => 'twillio/hello'
   end
   
+  def success
+    render : template => 'twillio/hello'
+  end
 end
